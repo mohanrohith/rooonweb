71cd4b592e587c546584bfde6239c320f70060e2
Rohith Mohan <rohith.mohan@flipkart.com>
2015-02-05 20:15:29 +0530

    Added manifest to index

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>Rooonweb</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <link rel="manifest" href="manifest.json">
 </head>
 <body>
     Some cool is coming up
